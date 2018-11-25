43d0088eb6072cdbd30d03b235afef7f5ea17acf
ArekkuusuJerii <edgargcx99@gmail.com>
2018-11-25 15:13:34 -0600

    Add ORM Fixtures

---
+++ src/BackendBundle/Entity/Fixtures/AppFixtures.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pride White
+ * Date: 11/25/2018
+ * Time: 2:39 PM
+ */
+
+namespace BackendBundle\Entity\Fixtures;
+
+
+use BackendBundle\Entity\Persona;
+use BackendBundle\Entity\Usuario;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderAwareInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class AppFixtures implements FixtureInterface, EncoderAwareInterface
+{
+    /**
+     *
+     * @var ContainerAwareInterface
+     */
+    private $encoder_factory;
+
+    /**
+     * Set the ContainerAwareInteface method to load any services for data fixture classes
+     *
+     * {@inheritDoc}
+     * @see \Symfony\Component\DependencyInjection\ContainerAwareInterface::setContainer()
+     */
+    public function __construct(EncoderFactoryInterface $encoder_factory)
+    {
+        $this->encoder_factory = $encoder_factory;
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $persona = new Persona();
+        $persona->setNombre("Admin");
+        $persona->setApellidop("Mr");
+        $persona->setApellidom("Dick");
+        $usuario = new Usuario();
+        $usuario->setUser("admin@g.c");
+
+        $encoder = $this->encoder_factory->getEncoder($usuario);
+        $password = $encoder->encodePassword($usuario->getPassword(), $usuario->getSalt());
+
+        $usuario->setPassword($password);
+        $usuario->setPersona($persona);
+        $usuario->setRol("ROLE_ADMIN");
+        $manager->persist($usuario);
+        $manager->flush();
+    }
+
+    /**
+     * Gets the name of the encoder used to encode the password.
+     *
+     * If the method returns null, the standard way to retrieve the encoder
+     * will be used instead.
+     *
+     * @return string
+     */
+    public function getEncoderName()
+    {
+        return 'security.encoder_factory';
+    }
+}
--- src/BackendBundle/Entity/Usuario.php
+++ src/BackendBundle/Entity/Usuario.php
@@ -3,6 +3,7 @@
 namespace BackendBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Usuario
@@ -10,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="usuario", indexes={@ORM\Index(name="fk_Usuario_Persona1_idx", columns={"persona"})})
  * @ORM\Entity
  */
-class Usuario
+class Usuario implements \Serializable, UserInterface
 {
     /**
      * @var integer
@@ -45,7 +46,7 @@ class Usuario
     /**
      * @var Persona
      *
-     * @ORM\ManyToOne(targetEntity="Persona")
+     * @ORM\ManyToOne(targetEntity="Persona",cascade={"persist"})
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="persona", referencedColumnName="id")
      * })
@@ -157,5 +158,94 @@ class Usuario
     public function getPersona()
     {
         return $this->persona;
+    }
+
+    /**
+     * String representation of object
+     * @link https://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->user,
+            $this->password,
+            $this->rol,
+            $this->persona,
+        ));
+    }
+
+    /**
+     * Constructs the object
+     * @link https://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->id,
+            $this->user,
+            $this->password,
+            $this->rol,
+            $this->persona,
+            ) = unserialize($serialized);
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return array('ROLE_USER');
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return array($this->rol);
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
